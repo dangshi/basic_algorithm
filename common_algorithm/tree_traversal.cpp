cb173f05f8e18ede9a266267f630cd1e8bb56099
lan <lan@MacBook-Pro-2.local>
2020-08-17 16:29:20 +0800

    tree traversal

--- common_algorithm/tree_traversal.cpp
+++ common_algorithm/tree_traversal.cpp
@@ -1,4 +1,7 @@
-
+/* refer
+https://www.cnblogs.com/anniekim/archive/2013/06/15/morristraversal.html   morris
+https://segmentfault.com/a/1190000016674584    
+*/
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -85,6 +88,7 @@ public:
                 }
                 if(pre->right == NULL){
                     nodes.push_back(root->val);
+                    pre->right = root;
                     root = root->left;
                 }else{
                     pre->right = NULL;
@@ -107,8 +111,96 @@ public:
     }
 
     vector<int> iterativePreOrder(TreeNode* root){
-        
+        vector<int> nodes;
+        stack<TreeNode*> st;
+        while(root || !st.empty()){
+            if(root){
+                nodes.push_back(root->val);
+                st.push(root);
+                root = root->left;
+            }else{
+                root = st.top();
+                st.pop();
+                root = root->right;
+            }       
+        }
+        return nodes;
     }
 };
 
+class PostTreeTraversal{
+public:
+    vector<int> dfsTraversal(TreeNode* root){
+        vector<int> nodes;
+        dfs(nodes, root);
+    }
+    void dfs(vector<int>& nodes, TreeNode* root){
+        if(!root) return;
+        dfs(nodes, root->left);
+        dfs(nodes, root->right);
+        nodes.push_back(root->val);
+    }
 
+    vector<int> iterativePost(TreeNode* root){
+        vector<int> nodes;
+        stack<TreeNode* > st;
+        TreeNode* pre = NULL;
+
+        while(root || !st.empty()){
+            while(root){
+                st.push(root);
+                root = root->left;
+            }
+            root = st.top();
+            if(root->right == NULL || root->right == pre){
+                nodes.push_back(root->val);
+                st.pop();
+                pre = root;
+                root = NULL;
+            }else{
+                root = root->right;
+            }
+        }
+        return nodes;
+    }
+
+    void addReverse(vector<int>& nodes, TreeNode* s, TreeNode* e){
+        vector<int> tmp;
+        while(s){
+            tmp.push_back(s->val);
+            if(s == e){
+                break;
+            }else{
+                s = s->right;
+            }
+        }
+        nodes.insert(nodes.end(), tmp.rbegin(), tmp.rend());
+    }
+
+    vector<int> morrisPost(TreeNode* root){
+        TreeNode* dummy = new TreeNode(0);
+        dummy->left = root;
+        TreeNode* pre = NULL;
+        vector<int> nodes;
+        root = dummy;
+        while(root){
+            if(root->left == NULL){
+                root = root->right;
+            }else{
+                pre = root->left;
+                while(pre->right && pre->right != root){
+                    pre = pre->right;
+                }
+                if(pre->right == NULL){
+                    pre->right = root;
+                    root = root->left;
+                }else{
+                    addReverse(nodes, root->left, pre);
+                    pre->right = NULL;
+                    root = root->right;
+                }
+            }
+        }
+        return nodes;
+    }
+};
